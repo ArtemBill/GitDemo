4eac10d6bb692ffd756be2764e3c6b6b8ff7ef32
Artem <artem.lincoln.bilyk@gmail.com>
2022-08-15 15:48:53 +0300

    change Service to Options

--- test_jenkins.py
+++ test_jenkins.py
@@ -1,24 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# from selenium.webdriver.chrome.options import Options
-#
-# options = Options()
-# options.add_argument("--headless")
-# options.add_argument("window-size=1400,1500")
-# options.add_argument("--disable-gpu")
-# options.add_argument("--no-sandbox")
-# options.add_argument("start-maximized")
-# options.add_argument("enable-automation")
-# options.add_argument("--disable-infobars")
-# options.add_argument("--disable-dev-shm-usage")
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
 
 
-# driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 
-s = Service("venv/lib/python3.8/site-packages/selenium/webdriver/remote/webdriver.py")
-driver = webdriver.Chrome(service=s)
+# s = Service("/usr/bin/chromedriver")
+# driver = webdriver.Chrome(service=s)
 driver.get('https://rahulshettyacademy.com/angularpractice/')
 
 driver.find_element(by=By.CSS_SELECTOR, value=".btn-success").click()
