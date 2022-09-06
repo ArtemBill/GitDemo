33d426f9c74f670cbcdd8100353446858c270e57
Artem <artem.lincoln.bilyk@gmail.com>
2022-09-06 13:17:17 +0300

    remove path to chromdriver

--- test_AjaxLoader.py
+++ test_AjaxLoader.py
@@ -1,12 +1,26 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
-s = Service("/Users/soprano/Desktop/First_Module/pythonSelenium/chromedriver ")
-driver = webdriver.Chrome(service=s)
+options = Options()
+options.add_argument("--headless")
+options.add_argument("window-size=1400,1500")
+options.add_argument("--disable-gpu")
+options.add_argument("--no-sandbox")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+
+
+driver = webdriver.Chrome(options=options)
+
+# s = Service("/Users/soprano/Desktop/First_Module/pythonSelenium/chromedriver ")
+# driver = webdriver.Chrome(service=s)
 
 
 driver.get("http://webdriveruniversity.com/index.html")
